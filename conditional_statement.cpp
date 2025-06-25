be905818e05538fdf45115b37ca9e6d11b218b22
Dino7354 <strokeboy24@gmail.com>
2025-06-25 19:06:25 +0530

    voter system using if else statement

--- conditional_statement.cpp
+++ conditional_statement.cpp
@@ -1,16 +1,29 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    if (num <=0) {
-        cout << "The number is negitive." << endl;
-    } else {
-        cout << "The number is positive." << endl;
-    }
-    {
-        cout << "This is a conditional statement." << endl;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     if (num <=0) {
+//         cout << "The number is negitive." << endl;
+//     } else {
+//         cout << "The number is positive." << endl;
+//     }
+//     {
+//         cout << "This is a conditional statement." << endl;
+//     }
     
-}
+// }
+
+#include <iostream>
+using namespace std;
+ int main() {
+    int age;
+    cout <<"Enter your age: ";
+    cin >> age;
+    if (age <= 18){
+        cout << "You cannot vote it." << endl;
+    } else {
+        cout << "You can vote it" << endl;
+    }
+ }
