14cfb86d216888eac2169b0ebddb20961f90226f
Dino7354 <strokeboy24@gmail.com>
2025-06-25 19:02:00 +0530

    Add conditional statement program to evaluate positive and negative numbers

---
+++ conditional_statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (num <=0) {
+        cout << "The number is negitive." << endl;
+    } else {
+        cout << "The number is positive." << endl;
+    }
+    {
+        cout << "This is a conditional statement." << endl;
+    }
+    
+}
