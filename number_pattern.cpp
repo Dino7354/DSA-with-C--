6fa882f0363169fe8eee3b5d83309edd4d3eb140
Dino7354 <strokeboy24@gmail.com>
2025-06-28 17:47:16 +0530

    Refactor number pattern program to display characters instead of numbers; update output message for clarity

--- number_pattern.cpp
+++ number_pattern.cpp
@@ -1,20 +1,62 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, num;
+    
+//     cout<< "Enter the number: ";
+//     cin >> n;
+
+//     num=1;
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n;j++){
+//             cout << num;
+//             num++;
+
+//         }
+//         cout << endl;   
+//     }
+//     cout << "After the loops end the value of num: " << num << endl;
+//     return 0;
+// }
+
+// /*
+
+// output--
+
+// 1234
+// 5678
+// 9101112
+// 13141516
+// After the loops end the value of num: 17
+
+// */
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n, num;
-    
-    cout<< "Enter the number: ";
+    int n;
+    cout << "enter the number: ";
     cin >> n;
 
-    num=1;
-    for(int i=0; i<n; i++){
-        for(int j=0;j<n;j++){
-            cout << num;
-            num++;
+    char ch ='A';
 
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << ch;
+            ch=ch+1;
         }
-        cout << endl;   
+        cout << endl;
     }
-    cout << "After the loops end the value of num: " << num << endl;
-    return 0;
+    cout <<"After the completing the loop value of char is: " << ch << endl;
 }
+
+/*
+
+output--
+ABCD
+EFGH
+IJKL
+MNOP
+After the completing the loop value of char is: Q
+
+*/
