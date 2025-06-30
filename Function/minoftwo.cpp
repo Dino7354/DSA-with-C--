830f8bfea8c5f9007acc2fc4b9c97c2fa48a6808
Dino7354 <strokeboy24@gmail.com>
2025-06-30 14:39:50 +0530

    Add function to find the minimum of two integers
    - Implemented the minOfTwo function that takes two integers as input and returns the smaller of the two.
    - Added user input prompts for entering the two integers.
    - Displayed the result of the minimum value in the main function.

---
+++ Function/minoftwo.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//Function
+int minOfTwo(int a, int b){
+    if(a<b){
+        return a;
+    }else{
+        return b;
+    }
+}
+int main(){
+    int a,b;
+    cout << "enter the number a: ";
+    cin >> a;
+    cout << "enter the number b: ";
+    cin >> b;
+    //function call
+    cout << "min " << minOfTwo(a,b)<< endl;
+
+
+}
