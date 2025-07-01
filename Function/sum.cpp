f4f054ee738e19478e54d7cd5e6a0522d9cb53db
Dino7354 <strokeboy24@gmail.com>
2025-07-01 13:35:31 +0530

    Implement sum of digits function and nCr binomial coefficient calculation; add input handling in main

--- Function/sum.cpp
+++ Function/sum.cpp
@@ -1,15 +1,43 @@
+// #include<iostream>
+// using namespace std;
+
+// int sumN(int n){
+//     int sum=0;
+//     for(int i =1; i<=n; i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+
+// int main(){
+//     cout << sumN(5) << endl;
+//     cout << sumN(10) << endl;
+// }
+
+//calculate the sum of digit of a number.
 #include<iostream>
 using namespace std;
 
-int sumN(int n){
-    int sum=0;
-    for(int i =1; i<=n; i++){
-        sum+=i;
+int sumofdigit(int num){
+    int digitsum = 0;
+    while (num > 0)
+    {
+        int lastdigit = num % 10;
+        num/=10;
+         digitsum +=lastdigit;
     }
-    return sum;
+
+    return digitsum;
+    
 }
 
 int main(){
-    cout << sumN(5) << endl;
-    cout << sumN(10) << endl;
+    int sum;
+    cout << "Enter the value of sum: ";
+    cin >> sum;
+
+    cout << "sum = " <<sumofdigit(sum) << endl;
+
+    return 0;
+    
 }
