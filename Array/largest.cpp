53fa4d32478043450dd187b56262d68525c34a38
Dino7354 <strokeboy24@gmail.com>
2025-07-05 12:37:23 +0530

    Refactor largest number finding logic to improve clarity and add index output

--- Array/largest.cpp
+++ Array/largest.cpp
@@ -5,11 +5,17 @@ int main(){
     int num[] = {5, 15, 1, -15, 14, 2, -18};
     int size = 7;
     int largest = INT_MIN;
+    int index = -1;
 
     for(int i=0; i<size; i++){
-        largest = max(num[i], largest);
+        // largest = max(num[i], largest);
+        if(num[i] > largest){
+            largest = num[i];
+            index = i;
+        }
     }
 
     cout << "largest = " << largest << endl;
+    cout << "index = " << index << endl;
 
 }
