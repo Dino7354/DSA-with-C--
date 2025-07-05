6223ce5050bfec3b330e09b0ceab27eae949ff3d
Dino7354 <strokeboy24@gmail.com>
2025-07-05 12:38:44 +0530

    Add index output for smallest number in array

--- Array/smallest.cpp
+++ Array/smallest.cpp
@@ -5,13 +5,16 @@ int main(){
     int num[] = {5, 15, 1, -15, 14, 2, -18};
     int size = 7;
     int smallest = INT_MAX;
+    int index = 1;
 
     for(int i=0; i<size; i++){
         if(num[i] < smallest){
              smallest = num[i];
+             index = i;
         }
     }
 
     cout << "smallest = " << smallest << endl;
+    cout << "index = " << index << endl;
 
 }
