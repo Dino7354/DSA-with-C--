2a71a5e6eaf15e2d7db1f0fa511c5420d6a78573
Dino7354 <strokeboy24@gmail.com>
2025-07-07 10:52:20 +0530

    Update vector function examples to include pop_back and front demonstrations

--- vector/Vector_function.cpp
+++ vector/Vector_function.cpp
@@ -42,17 +42,30 @@ at;
 // }
 
 //Example of pop back vec function
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> vec={1,2,3,5};
+//     cout << "size of = " << vec.size() << endl;
+
+//     vec.pop_back();
+//     cout << "After pop back size = " << vec.size() << endl;
+
+//     for(int val : vec){
+//         cout << val << endl;
+//     }
+// }
+
+
+// Example of front vec function
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> vec={1,2,3,5};
-    cout << "size of = " << vec.size() << endl;
+    vector<int>vec={1,2,3,7,5,6};
+    cout <<"size = " << vec.size() << endl;
 
-    vec.pop_back();
-    cout << "After pop back size = " << vec.size() << endl;
+    cout << vec.front() << endl;
 
-    for(int val : vec){
-        cout << val << endl;
-    }
 }
