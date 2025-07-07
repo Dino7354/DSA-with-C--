8aadf0632e00b52fe2f8c80a1672c41f8b39b7fb
Dino7354 <strokeboy24@gmail.com>
2025-07-07 10:39:47 +0530

    Refactor vector function examples and update push_back demonstration

--- vector/Vector_function.cpp
+++ vector/Vector_function.cpp
@@ -8,16 +8,35 @@ back;
 at;
 */
 
+//Example of Size vec function
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> vec = {1, 2 , 5, 6};
+
+//     cout << "size = " << vec.size() << endl;
+
+//     // for eacch loop
+//     for(int val : vec){
+//         cout << val << endl;
+//     }
+// }
+
+//Example of push_back vec function
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> vec = {1, 2 , 5, 6};
+    vector<char> vec= {'a','b','c','d'};
 
     cout << "size = " << vec.size() << endl;
+    vec.push_back('e');
 
-    // for eacch loop
-    for(int val : vec){
+    cout << "After push back size of = " << vec.size() << endl;
+
+    for( char val : vec){
         cout << val << endl;
     }
+
 }
